7b561f6d8099b93acc60c9296e282a2c885f5140
firmm <matthew.firm@gmail.com>
2023-03-08 17:11:07 -0500

    Update post.js

--- src/components/post.js
+++ src/components/post.js
@@ -6,7 +6,7 @@ const Post = ({ id }) => {
 
   useEffect(() => {
     const getPost = async () => {
-      const resp = await fetch(`https://marketrisk.info/api/posts/${id}`);
+      const resp = await fetch(`https://api.marketrisk.info/api/posts/${id}`);
       const postResp = await resp.json();
       setPost(postResp);
     };
